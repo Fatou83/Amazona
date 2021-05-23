cd685d2eb153cd6b914728cedc5e9d2590a19127
Fatou83 <fatoumanendiaye83@gmail.com>
2021-05-23 15:26:17 +0000

    premiere version

---
+++ backend/data.js
@@ -0,0 +1,55 @@
+export default {
+    products:[{
+            _id: '1',
+            name: 'Pink shoes',
+            category: 'shoes',
+            image: '/images/img1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+    },
+    {
+        _id: '2',
+        name: 'T-shirt',
+        category: 'Shirts',
+        image: '/images/T1.jpg',
+        price: 70,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 8
+},
+
+{
+    _id: '3',
+    name: 'Pink shoes',
+    category: 'Shoes',
+    image: '/images/img1.jpg',
+    price: 60,
+    brand: 'Nike',
+    rating: 4.5,
+    numReviews: 13
+},
+{
+    _id: '4',
+    name: 'T-shirt',
+    category: 'Shirts',
+    image: '/images/T1.jpg',
+    price: 70,
+    brand: 'Nike',
+    rating: 4.5,
+    numReviews: 8
+},
+{
+    _id: '5',
+    name: 'Pink shoes',
+    category: 'shoes',
+    image: '/images/img1.jpg',
+    price: 50,
+    brand: 'Nike',
+    rating: 4.5,
+    numReviews: 10
+},
+
+]
+}
